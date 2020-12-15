f1fdcb3b7cfe890a572a046f4f0630c3bf0c1c37
Olga <example@mail.ru>
2020-12-15 21:00:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    name: "Olga",
+    age: 25,
+    isMarried: true
+};
+ console.log(obj.age);
